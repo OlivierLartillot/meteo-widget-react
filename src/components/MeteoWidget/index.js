28fd10f9f139a73253faef7646573031afcb38a8
OlivierLartillot <olivier.lartillot@gmail.com>
2024-02-20 17:59:51 +0100

    NEW: 📦 Css du composant meteo widget

--- src/components/MeteoWidget/index.js
+++ src/components/MeteoWidget/index.js
@@ -4,10 +4,9 @@ const MeteoWidget = ({city, zipcode}) => {
 
     return (
         <div className="meteo-widget">
-            <h1>Meteo widget</h1>
             <div className="meteo-widget__info">
-                <h1 className="meteo-widget__city">Avignon</h1>
-                <h2 className="meteo-widget__zipcode">84000</h2>
+                <h1 className="meteo-widget__info__city">Avignon</h1>
+                <h2 className="meteo-widget__info__zipcode">84000</h2>
             </div>
             <div className="meteo-widget__temperature">
                 12°
